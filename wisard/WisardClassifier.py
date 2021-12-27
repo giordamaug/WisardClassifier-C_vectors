805a754da3613e07e043316a431abd742d688137
Maurizio Giordano <maurizio.giordano@cnr.it>
2021-12-27 15:35:12 +0100

    Update WisardClassifier.py

--- wisard/WisardClassifier.py
+++ wisard/WisardClassifier.py
@@ -11,7 +11,7 @@ import time
 import multiprocessing as mp
 
 from wisard_wrapper import *
-from utilities import color
+from wisard.utilities import color
 
 mypowers = 2**np.arange(32, dtype = np.uint32)[::]
 
